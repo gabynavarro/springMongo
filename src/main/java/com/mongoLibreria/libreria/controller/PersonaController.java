71678eaf5391a2842011042a6482f613c77a3fbc
gabynavarro <alt64b@gmail.com>
2022-05-12 17:49:42 -0300

    Class Persona y Controllador

---
+++ src/main/java/com/mongoLibreria/libreria/controller/PersonaController.java
@@ -0,0 +1,44 @@
+package com.mongoLibreria.libreria.controller;
+
+import com.mongoLibreria.libreria.model.Persona;
+import com.mongoLibreria.libreria.service.PersonaService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1")
+@CrossOrigin("*")
+public class PersonaController {
+    private final PersonaService personaService;
+    
+    @PostMapping ("/add") 
+    public void save(@RequestBody Persona persona){
+        personaService.save(persona);
+    }
+    @GetMapping("/list")
+    public List<Persona> findAll(){
+        return personaService.findAll();
+    }
+    @GetMapping("/{id}")
+    public Persona findById(@PathVariable String id){
+        return personaService.findById(id);
+    }
+    @DeleteMapping("/{id}")
+    public  void delteById(@PathVariable String id){
+        personaService.delete(id);
+    }
+    @PutMapping("/update")
+    public void update(@RequestBody Persona persona){
+        personaService.save(persona);
+    }
+}
--- src/main/java/com/mongoLibreria/libreria/service/PersonaService.java
+++ src/main/java/com/mongoLibreria/libreria/service/PersonaService.java
@@ -3,6 +3,7 @@ package com.mongoLibreria.libreria.service;
 
 import com.mongoLibreria.libreria.model.Persona;
 import com.mongoLibreria.libreria.repository.PersonaRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +14,16 @@ public class PersonaService {
     
     public void save(Persona persona){
         personaRepository.save(persona);
+    }
+    public List<Persona> findAll(){      
+        return personaRepository.findAll();
+        
+    }
+    
+    public  Persona findById(String id){
+        return personaRepository.findById(id).orElseThrow();
+    }
+    public void delete(String id) {
+         personaRepository.deleteById(id);
     }
 }
